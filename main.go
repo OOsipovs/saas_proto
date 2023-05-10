9fac8df38cdac5626a0747869828fdcee951ca89
olegs.osipovs <olegs.osipovs@gmail.com>
2023-05-10 22:55:52 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+)
+
+type Route struct {
+	Logger  bool
+	Tester  bool
+	Handler http.Handler
+}
+
+type App struct {
+	User    *Route
+	Billing *Route
+}
+
+type User struct{}
+
+func (u User) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var head string
+	head, r.URL.Path = shiftPath(r.URL.Path)
+	if head == "profile" {
+		u.Profile(w, r)
+		return
+	} else if head == "detail" {
+		head, _ := shiftPath(r.URL.Path)
+		i, err := strconv.Atoi(head)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, ctxUserID, i)
+		u.Detail(w, r.WithContext(ctx))
+		return
+	}
+
+	http.Error(w, "not found", http.StatusNotFound)
+}
+
+func (h *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var next *Route
+	var head string
+	head, r.URL.Path = shiftPath(r.URL.Path)
+	if len(head) == 0 {
+		next = &Route{
+			Logger: true,
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("home page"))
+			}),
+		}
+	} else if head == "user" {
+		var i interface{} = User{}
+		next = &Route{
+			Logger:  true,
+			Tester:  true,
+			Handler: i.(http.Handler),
+		}
+	} else {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+
+	if next.Logger {
+		next.Handler = h.log(next.Handler)
+	}
+
+	if next.Tester {
+		next.Handler = h.test(next.Handler)
+	}
+
+	next.Handler.ServeHTTP(w, r)
+}
+
+func main() {
+	app := &App{
+		User: &Route{
+			Logger: true,
+			Tester: true,
+		},
+		Billing: &Route{
+			Logger: true,
+			Tester: true,
+		},
+	}
+
+	http.ListenAndServe(":8080", app)
+}
