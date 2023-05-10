229f262f93ac2023e2ecb620a289247f3381213b
olegs.osipovs <olegs.osipovs@gmail.com>
2023-05-10 23:16:09 +0300

    Router sceleton added

--- main.go
+++ main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"path"
 	"strconv"
+	"strings"
+	"time"
+)
+
+type key int
+
+const (
+	ctxTestKey key = 1
+	ctxUserID      = 2
 )
 
 type Route struct {
 	Logger  bool
 	Tester  bool
 	Handler http.Handler
-}
-
-type App struct {
-	User    *Route
-	Billing *Route
 }
 
 type User struct{}
@@ -40,6 +47,27 @@ func (u User) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Error(w, "not found", http.StatusNotFound)
+}
+
+func (u User) Detail(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	v := ctx.Value(ctxTestKey)
+	id := ctx.Value(ctxUserID)
+	w.Write([]byte(fmt.Sprintf("value of conetxt is %s for user id %d", v, id)))
+}
+
+func shiftPath(p string) (head, tail string) {
+	p = path.Clean("/" + p)
+	i := strings.Index(p[1:], "/") + 1
+	if i <= 0 {
+		return p[1:], "/"
+	}
+	return p[1:i], p[i:]
+}
+
+type App struct {
+	User    *Route
+	Billing *Route
 }
 
 func (h *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -74,6 +102,22 @@ func (h *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	next.Handler.ServeHTTP(w, r)
+}
+
+func (h *App) log(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Println(time.Since(start), r.Method, r.URL.Path)
+	})
+}
+
+func (h *App) test(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, ctxTestKey, "hello world")
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func main() {
